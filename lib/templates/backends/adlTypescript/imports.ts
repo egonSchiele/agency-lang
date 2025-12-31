70f19877f7e1f7acea7c08599ecc017b97370021
Aditya Bhargava <adit@adit.io>
2025-12-31 17:26:39 -0600

    oops

--- lib/templates/backends/adlTypescript/imports.ts
+++ lib/templates/backends/adlTypescript/imports.ts
@@ -7,6 +7,7 @@ export const template = `import OpenAI from "openai";
 import { zodResponseFormat } from "openai/helpers/zod";
 import { z } from "zod";
 import * as readline from "readline";
+import fs from "fs";
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,

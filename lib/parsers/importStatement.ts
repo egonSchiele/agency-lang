2d456e71cc74b3d731ab15ad57895b87e77b00af
Aditya Bhargava <adit@adit.io>
2026-01-04 17:54:49 -0600

    import bug fix

--- lib/parsers/importStatement.ts
+++ lib/parsers/importStatement.ts
@@ -1,12 +1,27 @@
 import { ImportStatement } from "@/types/importStatement";
-import { capture, many1Till, oneOf, Parser, seqC, set, spaces, str } from "tarsec";
+import {
+  capture,
+  many1Till,
+  oneOf,
+  Parser,
+  seqC,
+  set,
+  spaces,
+  str,
+  trace,
+} from "tarsec";
+import { optionalSemicolon } from "./parserUtils";
 
-export const importStatmentParser: Parser<ImportStatement> = seqC(
-  set("type", "importStatement"),
-  str("import"),
-  spaces,
-  capture(many1Till(str(" from")), "importedNames"),
-  str(" from"),
-  spaces,
-  capture(many1Till(oneOf(";\n")), "modulePath"),
-)
+export const importStatmentParser: Parser<ImportStatement> = trace(
+  "importStatement",
+  seqC(
+    set("type", "importStatement"),
+    str("import"),
+    spaces,
+    capture(many1Till(str("from")), "importedNames"),
+    str("from"),
+    spaces,
+    capture(many1Till(oneOf(";\n")), "modulePath"),
+    optionalSemicolon
+  )
+);

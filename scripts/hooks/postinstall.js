87abe2eb230e37727ec461b161605d9f9ffbafa2
Aditya Bhargava <adit@adit.io>
2026-02-16 22:10:46 -0600

    postinstall

---
+++ scripts/hooks/postinstall.js
@@ -0,0 +1,13 @@
+const cyan = "\x1b[36m";
+const yellow = "\x1b[33m";
+const reset = "\x1b[0m";
+console.log(`${cyan}Welcome to${reset}`);
+console.log(`${yellow}
+ _______                              
+|   _   .-----.-----.-----.----.--.--.
+|.  1   |  _  |  -__|     |  __|  |  |
+|.  _   |___  |_____|__|__|____|___  |
+|:  |   |_____|                |_____|
+|::.|:. |                             
+\`--- ---'                             
+${reset}`);

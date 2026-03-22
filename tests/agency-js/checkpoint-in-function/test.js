284e9233601238f5f4bdc6c283a6f6d31f7b45f4
Aditya Bhargava <adit@adit.io>
2026-03-22 18:14:19 -0500

    reorg, getCheckpoint test

--- tests/agency-js/checkpoint-in-function/test.js
+++
@@ -1,9 +0,0 @@
-import { main } from "./agent.js";
-import { writeFileSync } from "fs";
-
-const result = await main();
-
-writeFileSync(
-  "__result.json",
-  JSON.stringify(result.data, null, 2),
-);

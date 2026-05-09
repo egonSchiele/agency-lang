b5b8192f0ad4289c81ffebe485207e5b915119c9
Aditya Bhargava <adit@adit.io>
2026-05-09 12:03:52 -0500

    save

--- packages/agency-lang/lib/symbolTable.ts
+++ packages/agency-lang/lib/symbolTable.ts
@@ -3,6 +3,7 @@ import * as path from "path";
 import { parseAgency } from "./parser.js";
 import type { AgencyConfig } from "./config.js";
 import type {
+  AgencyNode,
   AgencyProgram,
   FunctionParameter,
   VariableType,
